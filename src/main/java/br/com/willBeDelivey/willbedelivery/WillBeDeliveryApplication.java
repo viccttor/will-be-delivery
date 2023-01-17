0943ee4dae436f185a1644d3a5eac445f69957c5
Victor Henrique <vhsdm7@gmail.com>
2023-01-17 17:49:45 -0300

    Criado projeto

---
+++ src/main/java/br/com/willBeDelivey/willbedelivery/WillBeDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.willBeDelivey.willbedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillBeDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillBeDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/willBeDelivey/willbedelivery/WillBeDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.willBeDelivey.willbedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillBeDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
